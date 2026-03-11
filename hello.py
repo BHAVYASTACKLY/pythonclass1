4179a8933211a8d1a6f24115834695767a492d95
BHAVYASTACKLY <pidugubhavya@thestackly.com>
2026-03-11 20:18:08 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,108 @@
+#print formatting task-1
+print("laptop","mouse","keyboard", sep="|")
+print("Hello World Welcome python" ,end="")
+print("Hello World Welcome python" ,end="\n")
+
+
+#Task-2 variables
+name="Ravi"
+age=22
+city="Chennai"
+print(name,age,city,sep="-")
+
+# task -3 multiple Assignment 
+name="Meena"
+age=20
+student=True
+print(name,age,student)
+
+#task 4 indexing
+#Given:
+#word = "Python"
+#Print:
+#First letter
+#Third letter
+#Last letter
+word ="Python"
+print(word[0])
+print(word[2])
+print(word[5])
+
+#Task 5 – Arithmetic Operators
+#Calculate and print:
+print(25+10)
+print(50-20)
+print(8*5)
+print(100/10)
+print(10%3)
+print(2**4)
+print(20//3)
+
+#task 6 bodmas expression
+print(3+2*5**2)
+#5**2=25
+#2*25=50
+#3+50
+
+#Task 7 – Assignment Operator
+num =20
+num += 25
+print(num)
+
+num =100
+num /=10
+print(num)
+
+#task 8 comparsion operators
+print(10>5)
+print(20<15)
+print(5==5)
+print(10!=8)
+print(7>=7)
+print(6<=2)
+
+#task 9 string comparison
+a="apple"
+b="Apple"
+print(a==b)
+# false bcoz python is dynamtical case sensitive
+# a & A is not equal so false
+
+
+#Task 10 – Logical Operators
+print(10>5 and 5==5)#true
+print(5>10 or 10==10)#true
+print(not(5>2))#false
+
+#task 11-membership operator
+numbers=[10,20,30,40,50]
+print(20 in numbers)##output true
+print(60 in numbers)##output false
+print(30 not in numbers)##output false
+
+#task12- swap variables
+a = 10
+b = 20
+temp=a
+a=b
+b=temp
+print(a,b)#output 20 10
+
+#task 13 -bitwise XOR
+a=6
+b=3
+print(a^b)
+#6- 110
+#3- 011
+
+
+
+
+
+
+
+
+
+
+
+
